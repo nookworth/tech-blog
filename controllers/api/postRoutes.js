c6ac4c2c476b76393460dd3b5a72feb4aab0b861
nookworth <nooksack@gmail.com>
2022-11-20 11:37:45 -0700

    Progress on dashboard.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,4 +15,29 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        text: req.body.text,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!updatedPost) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
---
+++ public/js/dashboard.js
@@ -0,0 +1,115 @@
+var postModal = document.getElementById("post-modal");
+var updateModal = document.getElementById("update-modal");
+var addBtn = document.getElementById("add-post");
+var updateBtn = document.getElementById("update-post");
+var span = document.getElementsByClassName("close")[0];
+
+const savePost = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-title").value.trim();
+  const text = document.querySelector("#post-text").value.trim();
+
+  if (title && text) {
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to create post");
+    }
+  }
+};
+
+const updatePost = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-title").value.trim();
+  const text = document.querySelector("#post-text").value.trim();
+
+  if (title && text) {
+    const response = await fetch(`/api/posts/${post.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title,
+        text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to update post");
+    }
+  } else if (title && !text) {
+    const response = await fetch(`/api/posts/${post.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to update post");
+    }
+  } else if (text && !title) {
+    const response = await fetch(`/api/posts/${post.id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        text,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Failed to update post");
+    }
+  }
+};
+
+const closeModal = () => {
+  postModal.style.display = "none";
+  updateModal.style.display = "none";
+};
+
+addBtn.onclick = function () {
+  postModal.style.display = "block";
+};
+
+updateBtn.onclick = function () {
+  updateModal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == postModal) {
+    postModal.style.display = "none";
+  } else if (event.target == updateModal) {
+    updateModal.style.display = "none";
+  }
+};
+
+document.querySelector(".close").addEventListener("click", savePost);
+document.querySelector(".close").addEventListener("click", closeModal);
+document.querySelector("#update-post").addEventListener("click", updatePost);
+document.querySelector("#update-post").addEventListener("click", closeModal);
